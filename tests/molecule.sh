f1a1d20cb4c094c5fa21d4b99d1ca6a97450ac6b
Jeff Geerling <geerlingguy@mac.com>
2020-05-09 11:36:39 -0500

    Issue #101: More test environment debugging.

--- tests/molecule.sh
+++ tests/molecule.sh
@@ -4,7 +4,9 @@
 set -e
 
 # Make sure pip3 is available.
-sudo apt-get update && sudo apt-get install -y python3-pip
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-setuptools
+pip3 install --upgrade setuptools pip
 
 # Install dependencies.
 sudo pip3 install ansible molecule ansible-test yamllint docker
