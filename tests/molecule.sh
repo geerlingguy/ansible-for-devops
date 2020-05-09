35dc3b44432d4c1627b9bd6ccb7dc50dd83f17a7
Jeff Geerling <geerlingguy@mac.com>
2020-05-09 11:17:00 -0500

    Issue #101: More attempts to get pip installed.

--- tests/molecule.sh
+++ tests/molecule.sh
@@ -3,8 +3,11 @@
 # Molecule playbook tests.
 set -e
 
+# Make sure pip3 is available.
+sudo apt-get install -y python3-pip
+
 # Install dependencies.
-pip install ansible molecule ansible-test yamllint docker
+sudo pip3 install ansible molecule ansible-test yamllint docker
 
 # Run tests.
 cd ../molecule
