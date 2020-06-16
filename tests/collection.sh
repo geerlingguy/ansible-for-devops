ae2c7b8c4082db810ab2a53102b3f3d024b387bb
Jeff Geerling <geerlingguy@mac.com>
2020-06-16 15:10:47 -0500

    Issue #202: Try to run collection test from shell script instead.

---
+++ tests/collection.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Collection playbook tests.
+set -e
+
+# Make sure pip3 is available.
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-setuptools
+pip3 install --upgrade setuptools pip
+
+# Install dependencies.
+sudo pip3 install ansible
+
+cd collection
+
+# Run Ansible playbook.
+ansible-playbook -c local -i 'localhost,' main.yml
