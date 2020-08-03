7577ca35aa190c5060ac52dd03aee747cd287c80
Jeff Geerling <geerlingguy@mac.com>
2020-08-03 16:02:00 -0500

    Issue #221: Fix broken tests by using the right names.

--- tests/orchestration.sh
+++ tests/orchestration.sh
@@ -15,11 +15,11 @@ cd orchestration/scripts
 
 # Test Django app installation.
 docker run -d --name app -p 80:80 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro geerlingguy/docker-centos8-ansible
-./django-app.sh
+./app.sh
 
 # Test Django db installation.
 docker run -d --name db -p 3360:3360 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro geerlingguy/docker-centos8-ansible
-./django-db.sh
+./db.sh
 
 # Other tests from the book.
-./orchestration.sh
+./multi.sh
