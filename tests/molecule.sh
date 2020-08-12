6594566e036961d86e8474e31661c144376540df
Jeff Geerling <geerlingguy@mac.com>
2020-08-12 14:52:43 -0500

    Fix broken molecule test on older Travis CI default Docker version.

--- tests/molecule.sh
+++ tests/molecule.sh
@@ -8,6 +8,9 @@ sudo apt-get update
 sudo apt-get install -y python3-pip python3-setuptools
 pip3 install --upgrade setuptools pip
 
+# Upgrade Docker to work with docker-py.
+curl https://gist.githubusercontent.com/geerlingguy/ce883ad4aec6a5f1187ef93bd338511e/raw/36612d28981d92863f839c5aefe5b7dd7193d6c6/travis-ci-docker-upgrade.sh | sudo bash
+
 # Install dependencies.
 sudo pip3 install ansible molecule ansible-lint yamllint docker
 
