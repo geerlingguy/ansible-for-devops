28c2b5f4d5afc4a347f225884eeab87431ec915a
Jeff Geerling <geerlingguy@mac.com>
2020-08-31 11:10:15 -0500

    Attempt to fix pip3 installs of ansible and molecule.

--- tests/collection.sh
+++ tests/collection.sh
@@ -6,7 +6,6 @@ set -e
 # Make sure pip3 is available.
 sudo apt-get update
 sudo apt-get install -y python3-pip python3-setuptools
-pip3 install --upgrade setuptools pip
 
 # Install dependencies.
 pip3 install ansible
--- tests/molecule.sh
+++ tests/molecule.sh
@@ -6,7 +6,6 @@ set -e
 # Make sure pip3 is available.
 sudo apt-get update
 sudo apt-get install -y python3-pip python3-setuptools
-pip3 install --upgrade setuptools pip
 
 # Upgrade Docker to work with docker-py.
 curl https://gist.githubusercontent.com/geerlingguy/ce883ad4aec6a5f1187ef93bd338511e/raw/36612d28981d92863f839c5aefe5b7dd7193d6c6/travis-ci-docker-upgrade.sh | sudo bash
