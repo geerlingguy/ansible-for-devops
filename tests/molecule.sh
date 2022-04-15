b282c5dd75cdf73786528b377d7c137eac7fe4b1
Jeff Geerling <geerlingguy@mac.com>
2022-04-14 22:16:31 -0500

    Issue #453: Update some broken CentOS 8 references in the code examples.

--- tests/molecule.sh
+++ tests/molecule.sh
@@ -8,7 +8,7 @@ pip3 install ansible molecule[docker] ansible-lint yamllint docker
 
 cd molecule
 
-# Run default test (CentOS).
+# Run default test (Rocky Linux).
 molecule test
 
 # Run tests on Debian.
