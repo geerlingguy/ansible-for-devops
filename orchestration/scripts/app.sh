fa76a4aca0b7f6d4a654105f74172076b9cc62fd
Jeff Geerling <geerlingguy@mac.com>
2022-04-14 23:28:25 -0500

    Fixes #337: Fix error with django installation in chapter 3.

--- orchestration/scripts/app.sh
+++ orchestration/scripts/app.sh
@@ -5,7 +5,7 @@ set -e
 
 # Configure Django on app server.
 ansible app -b -m yum -a "name=python3-pip state=present"
-ansible app -b -m pip -a "name=django<4 state=present"
+ansible app -b -m pip -a "executable=pip3 name=django<4 state=present"
 
 # Check Django version.
 ansible app -a "python3 -m django --version"
