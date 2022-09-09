f78c6c300016f7496ce66e7e9fccf26349fda221
Jeff Geerling <geerlingguy@mac.com>
2022-09-08 23:30:17 -0500

    Fixes #501: Update some references from yum to dnf. Hopefully not breaking.

--- orchestration/scripts/app.sh
+++ orchestration/scripts/app.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Configure Django on app server.
-ansible app -b -m yum -a "name=python3-pip state=present"
+ansible app -b -m dnf -a "name=python3-pip state=present"
 ansible app -b -m pip -a "executable=pip3 name=django<4 state=present"
 
 # Check Django version.
