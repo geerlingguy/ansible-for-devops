a4b7cfb5fb4e536496aad02161910238167e757d
Jeff Geerling <geerlingguy@mac.com>
2023-06-17 21:57:54 -0500

    Fixes #531: systemd with molecule Docker containers requires cgroupns set to host and rw.

--- tests/orchestration.sh
+++ tests/orchestration.sh
@@ -9,11 +9,11 @@ pip3 install ansible
 cd orchestration/scripts
 
 # Test Django app installation.
-docker run -d --name app -p 80:80 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro geerlingguy/docker-rockylinux8-ansible
+docker run -d --name app -p 80:80 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cgroupns=host geerlingguy/docker-rockylinux8-ansible
 ./app.sh
 
 # Test Django db installation.
-docker run -d --name db -p 3360:3360 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro geerlingguy/docker-rockylinux8-ansible
+docker run -d --name db -p 3360:3360 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cgroupns=host geerlingguy/docker-rockylinux8-ansible
 ./db.sh
 
 # Other tests from the book.
